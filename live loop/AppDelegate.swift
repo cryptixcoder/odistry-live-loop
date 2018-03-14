2d750c5d103e8c6c86a39ca104a5c50b2d217048
Markus  Gray <syncwarellc@gmail.com>
2018-03-14 11:54:51 -0400

    Initial Commit

---
+++ live loop/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  live loop
+//
+//  Created by Markus  Gray on 3/14/18.
+//  Copyright © 2018 Markus  Gray. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ live loop/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  live loop
+//
+//  Created by Markus  Gray on 3/14/18.
+//  Copyright © 2018 Markus  Gray. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
